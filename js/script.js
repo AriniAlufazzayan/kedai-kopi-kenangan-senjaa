305c478abb2690b4ef42b2ff9a8ac6a097311023
ayin.aalufazzayan@gmail.com <ayin.aalufazzayan@gmail.com>
2026-01-04 08:10:46 +0700

    melakukan perubahan

--- js/script.js
+++ js/script.js
@@ -41,10 +41,3 @@ menus.forEach(menu => {
 `;
 });
 
-// toggle class active untuk shopping cart
-const shoppingCart = document.querySelector('.shopping-cart');
-document.querySelector('#shopping-cart-button').onclick = (e) => {
-    shoppingCart.classList.toggle('active');
-    e.preventDefault();
-};
-    
