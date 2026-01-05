1403dbac8801969a0f12d082c5ced6a5ea2947ba
ayin.aalufazzayan@gmail.com <ayin.aalufazzayan@gmail.com>
2026-01-05 15:02:30 +0700

    melakukan perubahan

--- js/script.js
+++ js/script.js
@@ -68,12 +68,3 @@ menus.forEach(menu => {
 `;
 });
 
-// modal box
-const itemDetailModal = document.querySelector('#item-detail-modal');
-const itemDetailButton = document.querySelector('.item-detail-button');
-
-itemDetailButton.onclick = (e) => {
-    itemDetailModal.style.display = 'flex';
-    e.preventDefault(); 
-
-};
